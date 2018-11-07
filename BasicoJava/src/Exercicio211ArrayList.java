81e659ae66f6977abd12108ffa80d0a368e9e364
supertsp <tiago.souza.pedroso@gmail.com>
2018-11-06 22:42:24 -0200

    adicionei exercicio com ArrayList

--- BasicoJava/src/Exercicio211ArrayList.java
+++ BasicoJava/src/Exercicio211ArrayList.java
@@ -2,46 +2,54 @@ import static javax.swing.JOptionPane.showInputDialog; //input
 import static javax.swing.JOptionPane.showMessageDialog; //output
 import static java.lang.Integer.parseInt; //String -> int
 import static java.lang.String.valueOf; // ?? -> String
+import java.util.*;
 
-public class Exercicio211 {
-
+public class Exercicio211ArrayList {
+    //método executável
     public static void main(String[] args) {
-        //int idadeConvidado = receberInt("Digite sua idade");
-        //exibir("" + idadeConvidado); //forma1
-        //exibir(valueOf(idadeConvidado)); //forma2
-
-        int qtdConvidados = 5; //deixar como 20
-        int[] idades = new int[qtdConvidados]; //começa em 0
-        String[] nomes = new String[qtdConvidados];
-
-        //recebendo dados pro array
+        List<Integer> idades = new ArrayList<>();
+        List<String> nomes = new ArrayList<>();
+        
+        int qtdConvidados = receberInt(
+                "Digite a qtd convidados na lista"
+        );
+        
         for (int contador = 0; contador < qtdConvidados; contador++) {
-            nomes[contador] = showInputDialog(
+            nomes.add(
+                showInputDialog(
                     "CONTROLE DE CONVIDADOS\n" +
                     "Digite o nome do " + 
-                            (contador + 1) + "º convidado"
+                    (contador + 1) + "º convidado"
+                )
             );
             
-            idades[contador] = receberInt(
+            idades.add(
+                receberInt(
                     "Agora...\n" +
-                    "Digite a idade do " + nomes[contador]
-            );
-        }
-
+                    "Digite a idade do " + 
+                    nomes.get(contador)
+                )
+            );            
+        }//for
+        
         //exibir o resultado
-        String resposta = "LISTA FINAL DE CONVIDADOS\n";
-        
-        for (int cont = 0; cont < idades.length; cont++) {
-            resposta += 
-                    "Nome: " + nomes[cont] +
-                    ", " + idades[cont] + " anos " +
-                    "   - Situação: " + 
-                    validaConvidado(idades[cont]) + "\n";            
+        String resposta = "LISTA FINAL DE CONVIDADOS\n";        
+        for (String nome : nomes) {
+            resposta += "Nome: " + nome + "    ";
         }
-        
+        resposta += "\n";
+        for (Integer idade : idades) {
+            resposta += "Idade: " + idade + "    ";
+        }
+        resposta += "\n";
+        for (Integer idade : idades) {
+            resposta += "Situação: " + 
+                validaConvidado(idade) + "    ";
+        }
         exibir(resposta);
-    }
-
+        
+    }//main
+    
     public static int receberInt(String mensagemProUsuario) {
         String aux = showInputDialog(mensagemProUsuario);
         try {
@@ -55,7 +63,7 @@ public class Exercicio211 {
     public static void exibir(String mensagemProUsuario) {
         showMessageDialog(null, mensagemProUsuario);
     }
-
+    
     public static String validaConvidado(int idadeConvidado) {
         String msg = "";
 
@@ -67,5 +75,5 @@ public class Exercicio211 {
 
         return msg;
     }
-
-}
+    
+}//class
