d17b4593c09cbffab00a31aae2f5a3d20b149e82
supertsp <tiago.souza.pedroso@gmail.com>
2019-03-08 22:37:24 -0300

    correção das prova

---
+++ ModulosNetBeans/NovoProjeto/src/novoprojeto/Executavel.java
@@ -0,0 +1,17 @@
+// <editor-fold defaultstate="collapsed" desc="Codigo::Cabecalho">
+package novoprojeto;
+
+import static lerexibir.Janela.*;
+
+public class Executavel {
+
+    public static void main(String[] args) // </editor-fold> 
+    {
+
+        //seu programa aqui
+        
+    }
+
+// <editor-fold defaultstate="collapsed" desc="Codigo::Rodape">    
+}
+// </editor-fold> 
